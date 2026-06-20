fdd45462b43b8af37aa7991a0f023e49786f7351
WJZ_P <1369727119@qq.com>
2026-06-20 22:48:38 +0800

    rust的这个&s和s，大有来头，编译器做了很多事情。。。

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::ops::Deref;
+
 fn main() {
     let s = String::from("hello,world!");
     say_hello(&s);
+    say_hello(&(*(s.deref())));//   这一行是上面那行的最终编译器执行的代码
+    //  因为s类型是String，这里引用，用&s传入的话，类型就是&String，和函数的入参不符合。
+    //  此时rust就会做隐式转换，尝试套*，变成&(*s)，因为*s的类型是str，就和入参的匹配了！
+    //  最后rust再做展开，变成&(*(s.deref()))。
     say_hello(&s[..]);
     say_hello(s.as_str());
 }
