981659d60dd3e543d5205fa891f1551e74eaa19e
WJZ_P <1369727119@qq.com>
2026-06-23 13:23:51 +0800

    learning

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ struct Rectangle {
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-
-    println!("rect1 is {:?}", rect1);
+    let mut collection : [i32; 5] = [1, 2, 3, 4, 5];
+    //let mut collection : [String; 5] = std::array::from_fn(|_| String::from("Hello, World!"));
+    for i in 0..collection.len() {
+        println!("{}", collection[i]);
+        let j = collection[i];
+        collection[i] = j;
+    }
+    let i = collection[1];
+    collection[1] = i;
+    println!("{:?}", collection);
 }
