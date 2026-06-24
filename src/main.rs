2a72f08ede85a03bffe7c129be5ee505a7ea1f4d
WJZ_P <1369727119@qq.com>
2026-06-24 18:47:45 +0800

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-
-    for item in list.iter() {
-        if item > largest {
-            largest = &item;
-        }
-    }
-
-    largest
-}
+use std::convert::TryInto;
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let a: i32 = 10;
+    let b: u16 = 100;
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let b_ = b.try_into()
+        .unwrap();
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    if a < b_ {
+        println!("Ten is less than one hundred.");
+    }
 }
