317c847ec829c1fb8eb4d955470c73bcd4a6668c
WJZ_P <1369727119@qq.com>
2026-06-24 16:58:51 +0800

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-fn add<T>(a:T, b:T) -> T {
-    a + b
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list.iter() {
+        if item > largest {
+            largest = &item;
+        }
+    }
+
+    largest
 }
 
 fn main() {
-    println!("add i8: {}", add(2i8, 3i8));
-    println!("add i32: {}", add(20, 30));
-    println!("add f64: {}", add(1.23, 1.23));
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
