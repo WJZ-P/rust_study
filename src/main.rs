7a4f5f3a492433181f1b11119a279fd7f16cdeb3
WJZ_P <1369727119@qq.com>
2026-06-27 10:00:45 +0800

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use std::fs::File;
 
+//  让我来实现一个简单的宏！
+
+#[macro_export]
+macro_rules! vec {
+    ($($x:expr), *) => {
+        let mut temp_vec = Vec::new();
+        $(
+        temp_vec.push($x);
+        )*
+        temp_vec
+    };
+}
+
 fn main() {
     let f = File::open("hello.txt")?;
 }
