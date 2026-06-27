9728e05bcfc256a0ce3b0a89cbb9b2eb450d584d
WJZ_P <1369727119@qq.com>
2026-06-27 09:16:59 +0800

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-#[derive(Debug)]
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
+use std::fs::File;
 
 fn main() {
-    let i;
-    {
-        let novel = String::from("Call me Ishmael. Some years ago...");
-        let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-        i = ImportantExcerpt {
-            part: first_sentence,
-        };
-    }
-    println!("{:?}",i);
+    let f = File::open("hello.txt")?;
 }
